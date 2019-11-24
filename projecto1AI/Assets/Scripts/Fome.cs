4d9de9feb6d7afe99439712906b2eec64b927736
FranciscoPires <frelvaspires@gmail.com>
2019-11-24 17:21:03 +0000

    fome basica (work in progress)

---
+++ projecto1AI/Assets/Scripts/Fome.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fome : MonoBehaviour
+{
+    public float start = 2.0f;
+    public float frequency = 5f;
+
+    public bool isInvoke = false;
+
+    private void Start() {
+       /* if(isInvoke == true) {
+            Invoke();
+            isInvoke = false;
+        }*/
+    }
+
+    public void FixedUpdate() {
+        if (isInvoke == true) {
+           Invoke();
+        }
+        if(isInvoke == false) {
+            CancelInvoke();
+        }
+    }
+
+    public void FomeFunction() {
+        Debug.Log("Fome");
+        isInvoke = false;
+    }
+
+    public void Invoke() {
+        InvokeRepeating("FomeFunction", start, frequency);
+        //isInvoke = false;
+    }
+
+}
--- projecto1AI/Assets/Scripts/Pathfinder.cs
+++ projecto1AI/Assets/Scripts/Pathfinder.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
 {
-    public GameObject goal;
+    public GameObject goal1;
+    public GameObject goal2;
     public NavMeshAgent agent;
     // Start is called before the first frame update
     void Start() {
-        goal = GameObject.FindWithTag("goal");
+        goal1 = GameObject.Find("goal1");
+        goal2 = GameObject.Find("goal2");
         agent = GameObject.FindWithTag("Agent").GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update() {
-        agent.destination = goal.transform.position;
+        agent.destination = goal2.transform.position;
     }
 }
