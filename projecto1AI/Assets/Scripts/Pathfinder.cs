da6a3d0f9c49b297278879baffbc0e20d2250d2e
FranciscoPires <frelvaspires@gmail.com>
2019-11-24 12:52:51 +0000

    nav mesh basica a funcionar

---
+++ projecto1AI/Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    public GameObject goal;
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start() {
+        goal = GameObject.FindWithTag("goal");
+        agent = GameObject.FindWithTag("Agent").GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        agent.destination = goal.transform.position;
+    }
+}
