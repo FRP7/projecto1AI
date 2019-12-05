679f4d4160b2efd5d73a7c26bf59714160bc3a5d
FranciscoPires <frelvaspires@gmail.com>
2019-12-05 14:40:23 +0000

    small fix no palco randozimado

---
+++ projecto1AI/Assets/Scripts/zonatrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zonatrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision) {
+        if(collision.gameObject.tag == "Agent") {
+            Debug.Log("Entrou");
+        }
+    }
+
+    private void OnTriggerExit(Collider leave) {
+        if(leave.gameObject.tag == "Agent") {
+            Debug.Log("Saiu");
+        }
+    }
+}
