9ef1302c78106363ee4cadebbcb4ea5ecf3511e0
FranciscoPires <frelvaspires@gmail.com>
2019-12-07 17:16:22 +0000

    comportamento no jardim feito (kinda)
    Não consegui fazer exactamente como o professor queria por isso, fiz de uma forma diferente (com random) e caso consiga arranjar maneira de fazer como o professor mudo. Por agora está fechado.

--- projecto1AI/Assets/Scripts/JardimBehaviour.cs
+++ projecto1AI/Assets/Scripts/JardimBehaviour.cs
@@ -7,25 +7,50 @@ public class JardimBehaviour : MonoBehaviour
 {
     public int JardimState;
     public NavMeshAgent NavAgent;
-    public int[] agentnumber;
+    // public int[] agentnumber;
+    /*public int zona1num;
+    public int zona2num;*/
     public GameObject zona1;
+    public GameObject zona2;
+    public GameObject zona3;
+    public GameObject zona4;
+    public GameObject zona5;
+    public GameObject zona6;
+    public GameObject zona7;
+    public GameObject zona8;
+    public GameObject zona9;
+    public GameObject zona10;
+    public GameObject zona11;
+    public GameObject zona12;
+    public GameObject zona13;
+    public GameObject zona14;
+    public GameObject zona15;
+    public GameObject zona16;
+
+    public int jardim;
 
     public void Start() {
         NavAgent = GetComponent<NavMeshAgent>();
         zona1 = GameObject.Find("zona1");
+        zona2 = GameObject.Find("zona2");
+        zona3 = GameObject.Find("zona3");
+        zona4 = GameObject.Find("zona4");
+        zona5 = GameObject.Find("zona5");
+        zona6 = GameObject.Find("zona6");
+        zona7 = GameObject.Find("zona7");
+        zona8 = GameObject.Find("zona8");
+        zona9 = GameObject.Find("zona9");
+        zona10 = GameObject.Find("zona10");
+        zona11 = GameObject.Find("zona11");
+        zona12 = GameObject.Find("zona12");
+        zona13 = GameObject.Find("zona13");
+        zona14 = GameObject.Find("zona14");
+        zona15 = GameObject.Find("zona15");
+        zona16 = GameObject.Find("zona16");
+
+        jardim = Random.Range(0, 16);
     }
 
-    /* private void OnTriggerStay(Collider other) {
-         if (other.gameObject.name == "Jardim1") {
-             JardimState = 1;
-         } 
-     }*/
-
-    /* private void OnTriggerExit(Collider other) {
-         if(other.gameObject.name == "Jardim1") {
-             JardimState = 2;
-         }
-     }*/
 
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.name == "Jardim1") {
@@ -39,7 +64,6 @@ public class JardimBehaviour : MonoBehaviour
         switch(JardimState) {
             case 1:
                 GoToZone();
-                //Debug.Log("Jardim behaviour");
                 break;
             case 2:
                 break;
@@ -50,8 +74,57 @@ public class JardimBehaviour : MonoBehaviour
     }
 
     public void GoToZone() {
-        NavAgent.destination = zona1.transform.position;
-    }
+        //NavAgent.destination = zona1.transform.position;
+        if (jardim == 0) {
+            NavAgent.destination = zona1.transform.position;
+        }
+        if (jardim == 1) {
+            NavAgent.destination = zona2.transform.position;
+        }
+        if (jardim == 2) {
+            NavAgent.destination = zona3.transform.position;
+        }
+        if (jardim == 3) {
+            NavAgent.destination = zona4.transform.position;
+        }
+        if (jardim == 4) {
+            NavAgent.destination = zona5.transform.position;
+        }
+        if (jardim == 5) {
+            NavAgent.destination = zona6.transform.position;
+        }
+        if (jardim == 6) {
+            NavAgent.destination = zona7.transform.position;
+        }
+        if (jardim == 7) {
+            NavAgent.destination = zona8.transform.position;
+        }
+        if (jardim == 8) {
+            NavAgent.destination = zona9.transform.position;
+        }
+        if (jardim == 9) {
+            NavAgent.destination = zona10.transform.position;
+        }
+        if (jardim == 10) {
+            NavAgent.destination = zona11.transform.position;
+        }
+        if (jardim == 11) {
+            NavAgent.destination = zona12.transform.position;
+        }
+        if (jardim == 12) {
+            NavAgent.destination = zona13.transform.position;
+        }
+        if (jardim == 13) {
+            NavAgent.destination = zona14.transform.position;
+        }
+        if (jardim == 14) {
+            NavAgent.destination = zona15.transform.position;
+        }
+        if (jardim == 15) {
+            NavAgent.destination = zona16.transform.position;
+        }
+        return;
 
+    }
 
 }
--- projecto1AI/Assets/Scripts/zonatrigger.cs
+++ projecto1AI/Assets/Scripts/zonatrigger.cs
@@ -7,31 +7,91 @@ public class zonatrigger : MonoBehaviour
 {
     public bool zona1;
     public bool zona2;
-    public JardimBehaviour JardimBehaviourinstance;
+    public bool zona3;
+    public bool zona4;
+    public bool zona5;
+    public bool zona6;
+    public bool zona7;
+    public bool zona8;
+    public bool zona9;
+    public bool zona10;
+    public bool zona11;
+    public bool zona12;
+    public bool zona13;
+    public bool zona14;
+    public bool zona15;
+    public bool zona16;
+    //public JardimBehaviour JardimBehaviourinstance;
     public NavMeshAgent NavAgent;
 
     public void Awake() {
-        JardimBehaviourinstance = GameObject.FindWithTag("Agent").GetComponent<JardimBehaviour>();
+        //JardimBehaviourinstance = GameObject.FindWithTag("Agent").GetComponent<JardimBehaviour>();
         NavAgent = GameObject.FindWithTag("Agent").GetComponent<NavMeshAgent>();
     }
 
     private void OnTriggerEnter(Collider collision) {
         if(collision.gameObject.tag == "Agent" && zona1 == true) {
-            JardimBehaviourinstance.agentnumber[0] += 1;
+            //JardimBehaviourinstance.agentnumber[0] += 1;
+            //JardimBehaviourinstance.zona1num += 1;
             NavAgent.ResetPath();
         }
         if (collision.gameObject.tag == "Agent" && zona2 == true) {
-            JardimBehaviourinstance.agentnumber[1] += 1;
+            //JardimBehaviourinstance.agentnumber[1] += 1;
+            //JardimBehaviourinstance.zona2num += 1;
+            NavAgent.ResetPath();
+        }
+        if (collision.gameObject.tag == "Agent" && zona3 == true) {
+            NavAgent.ResetPath();
+        }
+        if (collision.gameObject.tag == "Agent" && zona4 == true) {
+            NavAgent.ResetPath();
+        }
+        if (collision.gameObject.tag == "Agent" && zona5 == true) {
+            NavAgent.ResetPath();
+        }
+        if (collision.gameObject.tag == "Agent" && zona6 == true) {
+            NavAgent.ResetPath();
+        }
+        if (collision.gameObject.tag == "Agent" && zona7 == true) {
+            NavAgent.ResetPath();
+        }
+        if (collision.gameObject.tag == "Agent" && zona8 == true) {
+            NavAgent.ResetPath();
+        }
+        if (collision.gameObject.tag == "Agent" && zona9 == true) {
+            NavAgent.ResetPath();
+        }
+        if (collision.gameObject.tag == "Agent" && zona10 == true) {
+            NavAgent.ResetPath();
+        }
+        if (collision.gameObject.tag == "Agent" && zona11 == true) {
+            NavAgent.ResetPath();
+        }
+        if (collision.gameObject.tag == "Agent" && zona12 == true) {
+            NavAgent.ResetPath();
+        }
+        if (collision.gameObject.tag == "Agent" && zona13 == true) {
+            NavAgent.ResetPath();
+        }
+        if (collision.gameObject.tag == "Agent" && zona14 == true) {
+            NavAgent.ResetPath();
+        }
+        if (collision.gameObject.tag == "Agent" && zona15 == true) {
+            NavAgent.ResetPath();
+        }
+        if (collision.gameObject.tag == "Agent" && zona16 == true) {
             NavAgent.ResetPath();
         }
     }
 
-    private void OnTriggerExit(Collider leave) {
+   /* private void OnTriggerExit(Collider leave) {
         if(leave.gameObject.tag == "Agent" && zona1 == true) {
-            JardimBehaviourinstance.agentnumber[0] -= 1;
+            //JardimBehaviourinstance.agentnumber[0] -= 1;
+            //JardimBehaviourinstance.zona1num -= 1;
         }
         if (leave.gameObject.tag == "Agent" && zona2 == true) {
-            JardimBehaviourinstance.agentnumber[1] -= 1;
+            //JardimBehaviourinstance.agentnumber[1] -= 1;
+            //JardimBehaviourinstance.zona2num -= 1;
         }
-    }
+    }*/
 }
