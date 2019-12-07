32f00f8f04a3d2feeb3edc6ed0df5cfe0431f3ef
FranciscoPires <frelvaspires@gmail.com>
2019-12-07 17:35:24 +0000

    small fix no gamemanager e bloqueio temporario
    tentei reduzir o atrito que o game manager faz ao disparar os agentes

--- projecto1AI/Assets/Scripts/GameManager.cs
+++ projecto1AI/Assets/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
     void Awake()
     {
         for(int i = 0; i < agentnumber; i++) {
-            Instantiate(agenteprefab, new Vector3(0f, 6f, 26.6f), Quaternion.identity);
+            Instantiate(agenteprefab, /*new Vector3(0f, 6f, 26.6f)*/ new Vector3(0f, 0f, 0f), Quaternion.identity);
         }
     }
 
