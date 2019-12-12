60263dd0f6566483b7a73d60c9dfa06ca8145cf5
Nuno <nunosilvares@hotmail.com>
2019-12-12 16:44:55 +0000

    Mudança do spawn dos agentes e Adicionar Elementos de UI

---
+++ projecto1AI/Assets/Scripts/Agente.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agente : MonoBehaviour
+{
+    public Agente agente;
+    public Fome fome;
+    public Descansar descansar;
+    public GameObject UIName;
+    public GameObject UIHunger;
+    public GameObject UIEnergy;
+
+    public string[] NomePrimeiro;
+    public string[] NomeSegundo;
+    public int HungerLevel;
+    public int RestLevel;
+
+
+    private void OnMouseDown()
+    {
+        UIName.GetComponent<TextMesh>().text = agente.name;
+        HungerLevel = (int)fome.HungerLevel;
+        UIHunger.GetComponent<TextMesh>().text = $"Fome :    {HungerLevel.ToString()}";
+        RestLevel = (int)descansar.RestLevel;
+        UIEnergy.GetComponent<TextMesh>().text = $"Energia : {RestLevel.ToString()}";
+    }
+}
--- projecto1AI/Assets/Scripts/GameManager.cs
+++ projecto1AI/Assets/Scripts/GameManager.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Transform agenteprefab;
-    public int agentnumber = 100;
+    public Transform[] agente;
+    public Agente AgenteP;
+
+    public string Name = "Default Name";
 
     void Awake()
     {
-        for(int i = 0; i < agentnumber; i++) {
-            Instantiate(agenteprefab, /*new Vector3(0f, 6f, 26.6f)*/ new Vector3(0f, 0f, 0f), Quaternion.identity);
+        for (int i = 0; i < agente.Length; i++)
+        {
+            agente[i] = Instantiate(agenteprefab, new Vector3(Random.Range(-19f, 19f), 1.4f, Random.Range(-2f, 55f)), Quaternion.identity);
+            Name = $"{AgenteP.NomePrimeiro[(int)Random.Range(0f, AgenteP.NomePrimeiro.Length)]} {AgenteP.NomeSegundo[(int)Random.Range(0f, AgenteP.NomeSegundo.Length)]}";
+            agente[i].name = Name;
         }
-    }
-
-    void Update()
-    {
-        
     }
 }
